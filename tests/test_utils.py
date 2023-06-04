9fae67047f020d930efc34d5b518a416ea00746b
Dominik Peter <dominik.peter@bmsuisse.ch>
2023-06-04 19:39:01 +0200

    add more test

---
+++ tests/test_utils.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.append(".")
+
+from bmsdna.lakeapi.core.yaml import get_yaml
+
+def test_load_yaml():
+
+    y = get_yaml("config_test.yml")
+    assert y.get("app").get("title") == "LakeAPI"
+
