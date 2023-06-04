105f2bb3968ecbb3039775ed18d5fd9d87a04412
Dominik Peter <dominik.peter@bmsuisse.ch>
2023-06-04 19:13:37 +0200

    enhance performance test

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -25,22 +25,35 @@ def test_async_execution():
 
     # api should not block
 
-    async def call_api(engine):
+    async def call_api_1(engine, format):
         start = time.time()
-        response = client.get(f"/api/v1/test/fake_delta?limit=10000&format=json&%24engine={engine}", auth=auth)
+        response = client.get(f"/api/v1/test/fake_delta?limit=10000&format=json&%24engine={engine}&format={format}", auth=auth)
         end = time.time()
         assert response.status_code == 200
 
         duration = end - start
-        print(f"Engine {engine} took {duration} seconds")
+        print(f"Engine {engine} took {duration} seconds with format {format}")
         assert end - start < 0.75
+
+
+    async def call_api_2(engine, format):
+        start = time.time()
+        response = client.get(f"/api/v1/test/fruits_partition?limit=10000&format=json&cars=audi&%24engine={engine}&format={format}", auth=auth)
+        end = time.time()
+        assert response.status_code == 200
+
+        duration = end - start
+        print(f"Engine {engine} took {duration} seconds with format {format}")
+        assert end - start < 1 # subsecond resuls needs to be acieved
 
     async def main():
         tasks = []
-        for _ in range(10_000):
+        for _ in range(100):
             for e in engines:
-                task = asyncio.create_task(call_api(e))
-                tasks.append(task)
+                for f in ("json", "csv", "scsv", "arrow", "parquet"):
+                    tasks.append(asyncio.create_task(call_api_1(e, f)))
+                    tasks.append(asyncio.create_task(call_api_2(e, f)))
+        print(f"Running {len(tasks)} tasks")
         await asyncio.gather(*tasks)
 
     asyncio.run(main())
