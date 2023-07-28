b24c0f2d27ef9afc0b5e87fa1b45c20afaa9a759
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2023-07-28 10:13:11 +0200

    allow skipping mssql for development

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,10 @@ def spawn_sql():
     import test_server
     import os
 
-    sql_server = test_server.start_mssql_server()
-    yield sql_server
-    if os.getenv("KEEP_SQL_SERVER", "0") == "0":  # can be handy during development
-        sql_server.stop()
+    if os.getenv("NO_SQL_SERVER", "0") == "1":
+        yield None
+    else:
+        sql_server = test_server.start_mssql_server()
+        yield sql_server
+        if os.getenv("KEEP_SQL_SERVER", "0") == "0":  # can be handy during development
+            sql_server.stop()
