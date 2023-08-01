6fea043936c9a41bbbd4038c5db2f2469204e9ce
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2023-08-01 07:48:25 +0200

    use duckdb's read_parquet for delta v2

--- bmsdna/lakeapi/delta/__init__.py
+++ bmsdna/lakeapi/delta/__init__.py
@@ -1,1 +1,1 @@
-from .colmapping import only_fixed_supported, get_pyarrow_dataset, get_pyarrow_table
+from .colmapping import only_fixed_supported, get_pyarrow_dataset, get_pyarrow_table, get_sql_for_delta
