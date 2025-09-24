73014fb38d69940361d2a2469482d89ed37f1708
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2025-09-24 11:50:14 +0200

    test

--- tests/test_blobb.py
+++ tests/test_blobb.py
@@ -15,6 +15,23 @@ def test_parquet(engine, client: TestClient):
 
 
 @pytest.mark.parametrize("engine", engines)
+def test_parquet_copied(engine, client: TestClient):
+    response = client.get(
+        f"/api/v1/blobb/blob_test_copied?format=json&limit=50&$engine={engine}",
+    )
+    assert response.status_code == 200
+    fakedt = response.json()
+    assert len(fakedt) == 50
+
+    response2 = client.get(
+        f"/api/v1/blobb/blob_test_copied?format=json&limit=50&$engine={engine}",
+    )
+    assert response2.status_code == 200
+    fakedt2 = response2.json()
+    assert len(fakedt2) == 50
+
+
+@pytest.mark.parametrize("engine", engines)
 def test_delta(engine, client: TestClient):
     response = client.get(
         f"/api/v1/blobb/fake?format=json&limit=50&$engine={engine}",
